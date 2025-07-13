55868179851700f4ab9c5a516f5ace77524974af
wuwenjincat <tomatocat1130@qq.com>
2025-07-13 13:01:42 +0800

    2025.7.13 提交修改

--- vue-examples-wwj/src/views/example02/CreditJisuan.vue
+++ vue-examples-wwj/src/views/example02/CreditJisuan.vue
@@ -3,61 +3,39 @@ import { listCourses } from '@/datasource/DataSource'
 import type { Course } from '@/datasource/Types'
 import { computed, ref, watch } from 'vue'
 
-const courses: Course[] = listCourses()
+const courses: Course[] = listCourses().sort((a, b) => a.term - b.term)
 
-const checkedCourseidsR = ref<number[]>([])
+const selectedCoursesR = ref<Course[]>([])
 
-const checkedCourses = computed(() =>
-  courses.filter(course => checkedCourseidsR.value.includes(course.id))
-)
-
-const totalScore = computed(() =>
-  checkedCourses.value.reduce((sum, course) => sum + course.credit, 0)
+const totalScoreC = computed(() =>
+  selectedCoursesR.value.reduce((sum, course) => sum + course.credit, 0)
 )
 
 const maxScore = 12
 
-const sortedCheckedCourses = computed(() =>
-  [...checkedCourses.value].sort((a, b) => a.term - b.term)
-)
-
-watch(checkedCourseidsR, (newIds, oldIds) => {
-  const addedIds = newIds.filter(id => !oldIds.includes(id))
-  const removedIds = oldIds.filter(id => !newIds.includes(id))
-
-  if (addedIds.length > 0) {
-    const addedNames = addedIds.map(id => courses.find(c => c.id === id)?.name || '无')
-    console.log(`新增课程: ${addedNames}`)
-  }
-
-  if (removedIds.length > 0) {
-    const removedNames = removedIds.map(id => courses.find(c => c.id === id)?.name || '无')
-    console.log(`移除课程: ${removedNames}`)
-  }
+watch(selectedCoursesR, newCourses => {
+  selectedCoursesR.value = newCourses.sort((a, b) => a.term - b.term)
 })
 </script>
 
 <template>
   <div class="container">
-    <div class="score-dispaly" :style="{ color: totalScore >= 12 ? 'green' : 'red' }">
-      {{ totalScore }}/{{ maxScore }}
+    <div class="score-dispaly" :style="{ color: totalScoreC >= 12 ? 'green' : 'red' }">
+      {{ totalScoreC }}/{{ maxScore }}
     </div>
     <div class="left">
       <h2>这是你可以选的课~</h2>
-      <div v-for="term in [4, 5, 6]" :key="term">
-        <h3>第{{ term }}学期</h3>
-        <div v-for="course in courses.filter(c => c.term === term)" :key="course.id">
-          <label>
-            <input type="checkbox" :value="course.id" v-model="checkedCourseidsR" />
-            {{ course.name }} ({{ course.credit }}学分)
-          </label>
-        </div>
+      <div v-for="course in courses" :key="course.id">
+        <label>
+          <input type="checkbox" :value="course" v-model="selectedCoursesR" />
+          {{ course.name }} - ({{ course.term }}) ({{ course.credit }}学分)
+        </label>
       </div>
     </div>
     <div class="right">
       <h2>这是你选好的课~</h2>
-      <div v-for="course in sortedCheckedCourses" :key="course.id">
-        <p>第{{ course.term }}学期-{{ course.name }} ({{ course.credit }}学分)</p>
+      <div v-for="course in selectedCoursesR" :key="course.id">
+        <p>{{ course.name }} - ({{ course.term }}) ({{ course.credit }}学分)</p>
       </div>
     </div>
   </div>

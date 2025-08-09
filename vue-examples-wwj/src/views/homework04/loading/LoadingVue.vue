be13261d203b4eb561ac7564e1b87fb23c76a978
wuwenjincat <tomatocat1130@qq.com>
2025-08-09 15:20:27 +0000

    update vue-examples-wwj/src/views/homework04/loading/LoadingVue.vue.
    Signed-off-by: wuwenjincat <tomatocat1130@qq.com>

--- vue-examples-wwj/src/views/homework04/loading/LoadingVue.vue
+++ vue-examples-wwj/src/views/homework04/loading/LoadingVue.vue
@@ -14,14 +14,10 @@ onUnmounted(() => {
 
 <template>
   <div class="loading">
-<<<<<<< HEAD
     <div class="loading-content">
       <img src="@/assets/whitecat_3.gif" alt="loading" class="loading-image" />
       <p class="loading-text">加载中...</p>
     </div>
-=======
-    <img style="margin: 200px auto; display: block" src="@/assets/whitecat_3.gif" alt="" />
->>>>>>> a16da92fd63c060498901191156b8b582c2af417
   </div>
 </template>
 
@@ -30,7 +26,6 @@ onUnmounted(() => {
   position: fixed;
   top: 0;
   left: 0;
-<<<<<<< HEAD
   width: 100vw;
   height: 100vh;
   background: linear-gradient(135deg, rgba(172, 197, 248, 0.3), rgba(231, 168, 199, 0.3));
@@ -54,9 +49,5 @@ onUnmounted(() => {
   margin-top: 16px;
   color: #333;
   font-size: 16px;
-=======
-  background: linear-gradient(135deg, rgba(172, 197, 248, 0.3), rgba(231, 168, 199, 0.3));
-  background-size: cover;
->>>>>>> a16da92fd63c060498901191156b8b582c2af417
 }
 </style>

272652a67226d2fcc76e6af521a60add6e3cdb9c
wuwenjincat <tomatocat1130@qq.com>
2025-08-09 16:07:13 +0000

    update vue-examples-wwj/src/views/homework04/loading/index.ts.
    Signed-off-by: wuwenjincat <tomatocat1130@qq.com>

--- vue-examples-wwj/src/views/homework04/loading/index.ts
+++ vue-examples-wwj/src/views/homework04/loading/index.ts
@@ -1,16 +1,13 @@
 import { createApp, h } from 'vue'
 import LoadingVue from './LoadingVue.vue'
 
-// 1. 首先定义返回类型接口
 interface LoadingInstance {
   close: () => void
 }
 
-// 2. 使用明确的类型替代ReturnType
 let loadingInstance: LoadingInstance | null = null
 
 export const createLoading = (): LoadingInstance => {
-  // 如果已存在实例，直接返回
   if (loadingInstance) {
     return loadingInstance
   }
